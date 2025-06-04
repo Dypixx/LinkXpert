0a6d08a79d6d9ac83b1a249216bd87514a0c750a
Dypixx <itsdypixx@gmail.com>
2025-06-04 18:27:09 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,18 +1,18 @@
 from os import getenv as genv
 
-API_ID = genv("API_ID", "27339145")
-API_HASH = genv("API_HASH", "e620f86919c34496729285dd2a6d35e6")
-BOT_TOKEN = genv("BOT_TOKEN", "7784529867:AAG9-hYNVY_g1bH28yCOxX_uavCNiehBXd8")
-ADMIN = int(genv("ADMIN", "1782834874"))
+API_ID = genv("API_ID", "")
+API_HASH = genv("API_HASH", "")
+BOT_TOKEN = genv("BOT_TOKEN", "")
+ADMIN = int(genv("ADMIN", ""))
 
 BASE_URL = genv("BASE_URL", "api.gplinks.com")
-DATABASE_URL = genv("DATABASE_URL", "mongodb+srv://starcinebot:mkooaa@werdeveloper.vxfam.mongodb.net/?retryWrites=true&w=majority&appName=werdeveloper")
+DATABASE_URL = genv("DATABASE_URL", "")
 
 IS_FSUB = bool(genv("FSUB", True))
-AUTH_CHANNELS = list(map(int, genv("AUTH_CHANNEL", "-1002421861644").split()))
+AUTH_CHANNELS = list(map(int, genv("AUTH_CHANNEL", "").split()))
 
 CHANNEL_LINK = genv("CHANNEL_LINK", "https://telegram.me/DypixxTech")
-REPO_LINK = genv("REPO_LINK", "https://telegram.me/DypixxTech")
+REPO_LINK = genv("REPO_LINK", "https://github.com/Dypixx/LinkXpert")
 
 
 START_TXT = '''<b>{},
